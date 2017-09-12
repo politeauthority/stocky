ac91c91055de93d006381145744e11adee22106d
Alix Fullerton <fullertonalix0@gmail.com>
2017-09-12 17:12:06 -0600

    Finalizing live run file, hopefully

--- run.sh
+++ run.sh
@@ -1,14 +1,7 @@
 if [ "${STOCKY_BUILD}" == "LIVE" ]; then
-  RUN_ENV="LIVE"
-else
-  RUN_ENV="${STOCKY_BUILD}"
-fi
-
-if [ RUN_ENV="DEV" ]; then
-	echo 'Running Standalone'
-	python run.py
-else
 	echo 'Running with Gunicorn'
 	gunicorn -w 4 -b 127.0.0.1:80 app:app
+else
+	echo 'Running Standalone'
+	python /opt/stocky/run.py
 fi
-echo 'Stopped'
