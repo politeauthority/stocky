64086eec57dff797814870e38fdae48bff4ad4f4
Alix Fullerton <alix@politeauthority.com>
2017-09-21 23:02:21 -0600

    Adding updates to company calculations when running year or daily fetch for companies

--- app/helpers/misc.py
+++ app/helpers/misc.py
@@ -1,4 +1,5 @@
-"""Misc
+"""Misc - Helpers
+
 """
 
 from datetime import datetime
@@ -8,6 +9,8 @@ def markets_open(check_time=None):
     """
     Determines if markets are currently open, or open by defined by the date supplied.
 
+    :param check_time: Datetime future or present.
+    :type check_time:
     :return: The markets are open or not.
     :rtype: bool
     """
@@ -16,12 +19,12 @@ def markets_open(check_time=None):
         the_time = check_time
     else:
         the_time = datetime.now()
-    import pdb
-    pdb.set_trace()
+
+    if the_time.weekday() in [5, 6]:
+        return False
+
     if the_time.hour + 2 > 9:
         return False
     if the_time.hour > 14:
-        return False
-    if the_time.strftime('%A') in ['Saturday', 'Sunday']:
         return False
     return True
